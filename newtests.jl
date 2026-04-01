dd21a277c886e923fa18f13967cccba784a5f4ec
Morgan <a12549445@unet.univie.ac.at>
2026-04-01 19:42:05 +0200

    TESTING NESTED REF AND ENRICH

--- newtests.jl
+++ newtests.jl
@@ -1,3 +1,15 @@
 ## Initial commit test
 
-println("hello world")
+#<step explain="OUTER STEP TESTING INNER STEP WITH REF">
+println("This is the outside of the step for ref")
+#<step explain="INNER STEP WITH REF" ref="https://google.com">
+println("This is the inside of the step, with a Google reference")
+#</step>
+#</step>
+
+#<step explain="OUTER STEP TESTING GEMINI INNER STEP">
+println("This is the outside of the step for Gemini")
+#<step explain="GEMINI INNER STEP" ref="https://google.com">
+println("This is the inside of the step, with a Google reference")
+#</step>
+#</step>
